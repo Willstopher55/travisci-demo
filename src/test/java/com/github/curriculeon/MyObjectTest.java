8e3c3c76453d33655d387268b327305d1fec60d3
willstopher817 <willstopher817@gmail.com>
2020-12-03 11:13:25 -0500

    bug fixed

--- src/test/java/com/github/curriculeon/MyObjectTest.java
+++ src/test/java/com/github/curriculeon/MyObjectTest.java
@@ -1,4 +1,34 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MyObjectTest {
+
+    @Test
+    public void test0() { // TODO
+        //Given
+        MyObject myObject = new MyObject();
+
+        //when
+        myObject.run();
+
+        //then
+        Assert.assertNotNull(myObject.toString());
+
+    }
+
+    @Test
+    public void test1() { // TODO
+        //Given
+        MyObject myObject = new MyObject();
+
+        //when
+        myObject.run();
+
+        //then
+        Assert.assertNotNull(myObject.toString());
+
+    }
+
 }
